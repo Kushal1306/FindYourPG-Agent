5e268fd41bed9adb4872d6c872e1abfb80c2f054
kushal1306 <kalakushal.jain@gmail.com>
2025-05-24 23:07:39 +0530

    trying to resolve cords issue

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const ENVIRONMENT = process.env.ENVIRONMENT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const app = express();
-app.use(express.json());
 
 const allowedOrigins =['http://localhost:5173','https://find-your-pg-agent.vercel.app'];
   // ENVIRONMENT === 'DEV'
@@ -25,9 +24,12 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.use(mainRouter);
 
